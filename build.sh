8d0fb45d48b21bfb998da50b346986f556bdd782
snehakpersistent <Sneha.Kanekar1@ibm.com>
2022-03-10 18:50:39 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/hashicorp/terraform.git
+cd terraform
+export TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout tags/$TAG
+
+env GOOS=linux GOARCH=ppc64le go build .
